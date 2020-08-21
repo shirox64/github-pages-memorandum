42c9bd29492d88174d22584bb3396a0611f0d0ff
shirox64 <shirox64@gmail.com>
2020-08-22 03:02:43 +0900

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -40,6 +40,15 @@ extensions = [
   "sphinx_markdown_tables"
 ]
 
+source_parsers = {
+    '.md': 'recommonmark.parser.CommonMarkParser',
+}
+
+source_suffix = {
+  '.rst': 'restructuredtext',
+  '.md': 'markdown'
+}
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
